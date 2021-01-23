aae3f42033849b5645206e3affdfdc3c30485f3b
FurkanKarakurt <furkankarakurt11@gmail.com>
2021-01-24 02:44:11 +0300

    first

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isim = new MyList<string>();
+
+            isim.Add("ahmet");
+
+        }
+    }
+}
